95a52409201b6280f7c87abb11283784c2a779ad
wangdong <20004604@qq.com>
2023-04-04 16:31:57 +0800

    优化代码

--- gittool.py
+++ gittool.py
@@ -28,12 +28,14 @@ class GitTool:
         self.branch_logger = None
         self.err_logger = None
         self.search_lib = None
+        # 时间限制
+        self.commit_since_before = None
 
     # 读取配置
     def read_config(self):
         # 读取配置文件
-        # with open('config_private.yml') as file:
-        with open('config.yml') as file:
+        with open('config_private.yml') as file:
+            # with open('config.yml') as file:
             config = yaml.safe_load(file)
 
         self.config = config
@@ -44,7 +46,6 @@ class GitTool:
         if self.config['model'] not in ['group', 'repository', 'group']:
             print("model 模式错误，只能是 group 群组模式，repository 单项目模式，repositories 多项目模式")
             exit()
-
         return config
 
     # 删除历史文件
@@ -214,16 +215,13 @@ class GitTool:
         result = json.loads(ctypes.string_at(c_result).decode('utf-8'))
         return result
 
-    # 群组模式
-    def _get_projects_by_model_group(self):
+    # 群组模式：过滤项目（将不需要处理的群组过滤掉）
+    def _filter_projects_by_group(self):
         # 根据群组ID拿到所有项目
         if self.config['type_group']['group_id'] is None:
             self.err_logger.info("群组项目ID（type_group.group_id）为空")
         group = self.gl.groups.get(self.config['type_group']['group_id'])
         projects = group.projects.list(all=True)
-
-        # 添加本次任务执行时间
-        logging.info(f"本次任务执行时间: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
         # 测试模式下只遍历第一个仓库
         if self.config['test_mode'] is True:
@@ -247,6 +245,200 @@ class GitTool:
 
         # projects_name_log = [project.name for project in projects]
         print("本次处理的项目有：", [project.name for project in projects])
+        return projects
+
+    # 群组模式：过滤分支（将不需要处理的分支过滤掉）
+    # 参数1：branches 指该项目下所有分支
+    # 参数2：repo 指项目（一个对象）
+    def _filter_branches_by_group(self, branches, repo):
+        selected_branches = []
+        # 如果匹配模式是全部分支
+        if self.config['type_group']['branch_match_type'] == "all":
+            print("当前寻找分支模式是：全部分支")
+            selected_branches = [branch.name for branch in branches]
+        elif self.config['type_group']['branch_match_type'] == "name_match":
+            print("当前寻找分支模式是：名称匹配")
+            all_branch_names = [branch.name for branch in branches]
+            for branch_name in all_branch_names:
+                # 解析字符串
+                branch_match_name = self.config['type_group']['branch_match_name']
+                # 避免不写情况，不写则默认为所有
+                if len(branch_match_name) == 0:
+                    err_msg = "错误：当 type_group.branch_match_type 为 name_match 时，type_group.branch_match_name 不能可为空。"
+                    print(err_msg)
+                    self.err_logger.info(err_msg)
+                    exit()
+
+                regex = re.compile(branch_match_name)
+                # 分支名进行匹配
+                if len(regex.findall(branch_name)) > 0:
+                    # 匹配成功则添加
+                    selected_branches.append(branch_name)
+        elif self.config['type_group']['branch_match_type'] == "last_commit_time":
+            # 存储每个分支的最新提交时间和分支名称的元组
+            branch_commits = []
+            # 按最后提交时间进行匹配
+            # 遍历所有分支
+            for branch in branches:
+                # print(f"正在获取分支：{branch.name} 的最新提交")
+                delta = datetime.timedelta(days=self.commit_since_before)
+                start_date = datetime.datetime.now() - delta
+                # 使用15天前的日期和当前日期作为日期范围
+                end_date = datetime.datetime.now()
+                # 获取该分支的最新提交，限制只返回1个
+                commit_list = repo.commits.list(ref_name=branch.name, since=start_date, until=end_date, get_all=True)
+                # print(f"总计有 {len(commit_list)} 个提交")
+                if len(commit_list) == 0:
+                    msg = f"分支：{branch.name} 最近{self.commit_since_before}天，无提交，跳过"
+                    print(msg)
+                    self.branch_logger.info(msg)
+                    continue
+                elif len(commit_list) > 20:
+                    msg = f"分支：{branch.name} 最近{self.commit_since_before}天，提交超过20次"
+                    print(msg)
+                    self.branch_logger.info(msg)
+                commit = commit_list[0]
+                # 将最新提交时间和分支名称作为一个元组添加到branch_commits列表中
+                branch_commits.append((commit.committed_date, branch.name))
+
+            # 按最近提交时间对分支进行排序，并提取排好序的分支名称。b[1] 指取 branch.name
+            selected_branches = [b[1] for b in sorted(branch_commits, reverse=True)]
+            print(f"所有分支排序后为：{selected_branches}")
+        else:
+            err_msg = "错误：当 model 为 group 时，未找到合法的 type_group.branch_match_type"
+            print(err_msg)
+            self.err_logger.info(err_msg)
+            exit()
+
+        # 此时拿到该项目下符合要求的分支，准备clone项目并进行处理
+        # 非全部分支模式下，则限定分支进行处理
+        if self.config['type_group']['branch_match_type'] != 'all' and self.config['type_group'][
+            'branch_limit'] != 'all':
+            if self.config['type_group']['branch_limit'] > 0:
+                print("目前限定处理分支数量是：", self.config['type_group']['branch_limit'])
+                # 则限制指定数量的分支数
+                selected_branches = selected_branches[:self.config['type_group']['branch_limit']]
+
+        return selected_branches
+
+    # 将项目clone到本地，然后进行处理（调用函数遍历项目查询字符串）
+    def _clone_and_deal(self, branch, project, repo):
+        logging.info(f"----- 小分割线 -----")
+        logging.info(f"正在处理分支：{branch}")
+        print(f"正在处理分支：{branch}")
+
+        # 检查目录是否存在，如果存在则删除
+        local_repo_path = f"{project.name}-{branch}"
+        local_repo_pathlib = pathlib.Path(local_repo_path)
+        if local_repo_pathlib.exists():
+            shutil.rmtree(local_repo_path, ignore_errors=True)
+
+        # 克隆仓库到本地临时目录
+        local_repo_path = f"{project.name}-{branch}"
+        Repo.clone_from(repo.http_url_to_repo, local_repo_path, branch=branch)
+        print(f"已克隆到本地：{local_repo_path}")
+
+        try:
+            print("开始获取所有匹配的文件")
+            # 从本地仓库获取文件列表
+            js_files = self.find_files_by_match_name(local_repo_path)
+
+            # 添加分支信息到 branch.log
+            local_repo = Repo(local_repo_path)
+            branch_commit = local_repo.commit(branch)
+            commit_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(branch_commit.committed_date))
+            branch_info = f"{repo.name}-{branch}-{branch_commit.hexsha}-{commit_time}-{branch_commit.author}"
+            self.branch_logger.info(branch_info)
+
+            print("开始遍历所有文件进行查询")
+            # 遍历所有匹配的文件
+            for js_file_path in js_files:
+                logging.info(f"正在处理文件：{os.path.abspath(os.path.join(local_repo_path, js_file_path))}")
+                print(f"正在处理文件：{os.path.abspath(os.path.join(local_repo_path, js_file_path))}")
+                # if os.path.abspath(os.path.join(local_repo_path,
+                #                                 js_file_path)) == "/Users/wangdong/githubProject/python3-lib/在git项目里寻找某个接口是否有调用/Special Transaction Center-CZP-TIP-864/src/api/maqian.js":
+                #     print("debug")
+                if self.config['file_search_engine'] == "python":
+                    # 使用 python 来查询文件
+                    search_result = self.search_string_in_file_by_python(
+                        os.path.join(local_repo_path, js_file_path),
+                        self.config['string_to_search'])
+                else:
+                    search_result = self.search_string_in_file_by_go(
+                        os.path.join(local_repo_path, js_file_path),
+                        self.config['string_to_search'])
+
+                if search_result['Error'] is not None:
+                    # 如果有报错信息，说明在查找该文件的时候出问题了
+                    self.err_logger.info(search_result['Error'])
+                else:
+                    # 此时正常，拿到匹配的行数
+                    matched_lines = search_result['MatchedLines']
+
+                    # 长度 > 0 ，说明有匹配的代码
+                    if len(matched_lines) > 0:
+                        for matched_line in matched_lines:
+                            match_info = f"{repo.name}-{branch}-{js_file_path}-{matched_line['LineNumber']}-{matched_line['Line'].strip()}"
+                            logging.info(match_info)
+                            # 写入 match.log
+                            self.match_logger.info(match_info)
+
+                            # 将匹配结果添加到 Excel 工作表
+                            self.worksheet.append([repo.name, branch, js_file_path, matched_line['LineNumber'],
+                                                   matched_line['Line'].strip()])
+
+
+        except Exception as e:
+            self.err_logger.error(f"项目：{project.name}，处理分支 {branch} 时出错：{e}")
+        finally:
+            # 删除本地仓库临时目录
+            shutil.rmtree(local_repo_path, ignore_errors=True)
+
+    # 群组模式
+    def _get_projects_by_model_group(self):
+        print("—————— 配置说明分割线（开始） ——————")
+        logging.info("—————— 配置说明分割线（开始） ——————")
+        msg = "本次处理的模式是：群组模式（group）"
+        logging.info(msg)
+        print(msg)
+        msg = f"当前的群组ID是：{self.config['type_group']['group_id']}"
+        logging.info(msg)
+        print(msg)
+        if self.config['type_group']['project_match_type'] == 'all':
+            msg = "项目筛选条件是：所有项目"
+        elif self.config['type_group']['project_match_type'] == 'normal':
+            msg = f"项目筛选条件是：名称匹配。关键词是：{self.config['type_group']['project_match_str']}"
+        elif self.config['type_group']['project_match_type'] == 'regexp':
+            msg = f"项目筛选条件是：正则表达式。正则表达式是：{self.config['type_group']['project_match_str']}"
+        logging.info(msg)
+        print(msg)
+
+        if self.config['type_group']['branch_match_type'] == 'all':
+            msg = f"当前分支匹配模式是：全部分支"
+            logging.info(msg)
+            print(msg)
+        elif self.config['type_group']['branch_match_type'] == 'last_commit_time':
+            # 计算n天前的日期
+            self.commit_since_before = self.config['type_group']['commit_since_before'] or 15
+            msg = f"当前寻找分支模式是：按最后提交时间。时间限制为最近{self.commit_since_before}天提交过的分支，然后对其进行排序，按提交顺序从近到远获取若干个分支进行处理。"
+            logging.info(msg)
+            print(msg)
+            msg = f"分支数为：{self.config['type_group']['branch_limit']}"
+            logging.info(msg)
+            print(msg)
+        elif self.config['type_group']['branch_match_type'] == 'name_match':
+            msg = f"当前寻找分支模式是：名称匹配。关键词为：{self.config['type_group']['branch_match_name']}"
+            logging.info(msg)
+            print(msg)
+            msg = f"分支数为：{self.config['type_group']['branch_limit']}"
+            logging.info(msg)
+            print(msg)
+
+        print("—————— 配置说明分割线（结束） ——————")
+        logging.info("—————— 配置说明分割线（结束） ——————")
+
+        # 先获取要处理哪些项目
+        projects = self._filter_projects_by_group()
 
         # 遍历项目
         for project in projects:
@@ -259,145 +451,15 @@ class GitTool:
             # 再获取到所有分支
             branches = repo.branches.list(get_all=True)
 
-            # 所有分支名
-            selected_branches = []
+            # 过滤分支，只获取指定分支
+            selected_branches = self._filter_branches_by_group(branches, repo)
 
-            # 如果匹配模式是全部分支
-            if self.config['type_group']['branch_match_type'] == "all":
-                print("当前寻找分支模式是：全部分支")
-                selected_branches = [branch.name for branch in branches]
-            elif self.config['type_group']['branch_match_type'] == "name_match":
-                print("当前寻找分支模式是：名称匹配")
-                all_branch_names = [branch.name for branch in branches]
-                for branch_name in all_branch_names:
-                    # 解析字符串
-                    branch_match_name = self.config['type_group']['branch_match_name']
-                    # 避免不写情况，不写则默认为所有
-                    if len(branch_match_name) == 0:
-                        err_msg = "错误：当 type_group.branch_match_type 为 name_match 时，type_group.branch_match_name 不能可为空。"
-                        print(err_msg)
-                        self.err_logger.info(err_msg)
-                        exit()
-
-                    regex = re.compile(branch_match_name)
-                    # 分支名进行匹配
-                    if len(regex.findall(branch_name)) > 0:
-                        # 匹配成功则添加
-                        selected_branches.append(branch_name)
-            elif self.config['type_group']['branch_match_type'] == "last_commit_time":
-                # 计算15天前的日期
-                commit_since_before = self.config['type_group']['commit_since_before'] or 15
-                print(f"当前寻找分支模式是：按最后提交时间。时间限制为最近{commit_since_before}提交过的分支")
-                # 存储每个分支的最新提交时间和分支名称的元组
-                branch_commits = []
-                # 按最后提交时间进行匹配
-                # 遍历所有分支
-                for branch in branches:
-                    # print(f"正在获取分支：{branch.name} 的最新提交")
-                    delta = datetime.timedelta(days=commit_since_before)
-                    start_date = datetime.datetime.now() - delta
-                    # 使用15天前的日期和当前日期作为日期范围
-                    end_date = datetime.datetime.now()
-                    # 获取该分支的最新提交，限制只返回1个
-                    commit_list = repo.commits.list(ref_name=branch.name, since=start_date, until=end_date)
-                    # print(f"总计有 {len(commit_list)} 个提交")
-                    if len(commit_list) == 0:
-                        print(f"分支：{branch.name} 最近{commit_since_before}天，无提交，跳过")
-                        continue
-                    commit = commit_list[0]
-                    # 将最新提交时间和分支名称作为一个元组添加到branch_commits列表中
-                    branch_commits.append((commit.committed_date, branch.name))
-
-                # 按最近提交时间对分支进行排序，并提取排好序的分支名称。b[1] 指取 branch.name
-                selected_branches = [b[1] for b in sorted(branch_commits, reverse=True)]
-                print(f"所有分支排序后为：{selected_branches}")
-            else:
-                err_msg = "错误：当 model 为 group 时，未找到合法的 type_group.branch_match_type"
-                print(err_msg)
-                self.err_logger.info(err_msg)
-                exit()
-
-            # 此时拿到该项目下符合要求的分支，准备clone项目并进行处理
-            # 非全部分支模式下，则限定分支进行处理
-            if self.config['type_group']['branch_match_type'] != 'all' and self.config['type_group']['branch_limit'] != 'all':
-                if self.config['type_group']['branch_limit'] > 0:
-                    print("目前限定处理分支数量是：", self.config['type_group']['branch_limit'])
-                    # 则限制指定数量的分支数
-                    selected_branches = selected_branches[:self.config['type_group']['branch_limit']]
-
-            print(f"总计{len(selected_branches)}个分支。")
+            msg = f"总计{len(selected_branches)}个分支。"
+            print(msg)
+            logging.info(msg)
             for branch in selected_branches:
-                logging.info(f"----- 小分割线 -----")
-                logging.info(f"正在处理分支：{branch}")
-                print(f"正在处理分支：{branch}")
-                # continue
-
-                # 检查目录是否存在，如果存在则删除
-                local_repo_path = f"{project.name}-{branch}"
-                local_repo_pathlib = pathlib.Path(local_repo_path)
-                if local_repo_pathlib.exists():
-                    shutil.rmtree(local_repo_path, ignore_errors=True)
-
-                # 克隆仓库到本地临时目录
-                local_repo_path = f"{project.name}-{branch}"
-                Repo.clone_from(repo.http_url_to_repo, local_repo_path, branch=branch)
-                print(f"已克隆到本地：{local_repo_path}")
-
-                try:
-                    print("开始获取所有匹配的文件")
-                    # 从本地仓库获取文件列表
-                    js_files = self.find_files_by_match_name(local_repo_path)
-
-                    # 添加分支信息到 branch.log
-                    local_repo = Repo(local_repo_path)
-                    branch_commit = local_repo.commit(branch)
-                    commit_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(branch_commit.committed_date))
-                    branch_info = f"{repo.name}-{branch}-{branch_commit.hexsha}-{commit_time}-{branch_commit.author}"
-                    self.branch_logger.info(branch_info)
-
-                    print("开始遍历所有文件进行查询")
-                    # 遍历所有匹配的文件
-                    for js_file_path in js_files:
-                        logging.info(f"正在处理文件：{os.path.abspath(os.path.join(local_repo_path, js_file_path))}")
-                        print(f"正在处理文件：{os.path.abspath(os.path.join(local_repo_path, js_file_path))}")
-                        # if os.path.abspath(os.path.join(local_repo_path,
-                        #                                 js_file_path)) == "/Users/wangdong/githubProject/python3-lib/在git项目里寻找某个接口是否有调用/Special Transaction Center-CZP-TIP-864/src/api/maqian.js":
-                        #     print("debug")
-                        if self.config['file_search_engine'] == "python":
-                            # 使用 python 来查询文件
-                            search_result = self.search_string_in_file_by_python(
-                                os.path.join(local_repo_path, js_file_path),
-                                self.config['string_to_search'])
-                        else:
-                            search_result = self.search_string_in_file_by_go(
-                                os.path.join(local_repo_path, js_file_path),
-                                self.config['string_to_search'])
-
-                        if search_result['Error'] is not None:
-                            # 如果有报错信息，说明在查找该文件的时候出问题了
-                            self.err_logger.info(search_result['Error'])
-                        else:
-                            # 此时正常，拿到匹配的行数
-                            matched_lines = search_result['MatchedLines']
-
-                            # 长度 > 0 ，说明有匹配的代码
-                            if len(matched_lines) > 0:
-                                for matched_line in matched_lines:
-                                    match_info = f"{repo.name}-{branch}-{js_file_path}-{matched_line['LineNumber']}-{matched_line['Line'].strip()}"
-                                    logging.info(match_info)
-                                    # 写入 match.log
-                                    self.match_logger.info(match_info)
-
-                                    # 将匹配结果添加到 Excel 工作表
-                                    self.worksheet.append([repo.name, branch, js_file_path, matched_line['LineNumber'],
-                                                           matched_line['Line'].strip()])
-
-
-                except Exception as e:
-                    self.err_logger.error(f"项目：{project.name}，处理分支 {branch} 时出错：{e}")
-                finally:
-                    # 删除本地仓库临时目录
-                    shutil.rmtree(local_repo_path, ignore_errors=True)
+                # 将项目clone到本地进行处理
+                self._clone_and_deal(branch, project, repo)
 
     # 单项目模式
     def _get_projects_by_model_repository(self):
